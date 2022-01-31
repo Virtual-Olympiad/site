678f6c5002cea44cb04e0c545c935a9d00fc8ab9
1egend <Charles.ran9@gmail.com>
2022-01-31 10:20:35 -0500

    Svelte Init

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<main>
+	<h1>Virtual Olympiad</h1>
+	<p>STEM Olympiad Organization aiming to help students learn and have fun</p>
+
+	<div>
+		Trainer
+	</div>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;	
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	div { 
+		
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
